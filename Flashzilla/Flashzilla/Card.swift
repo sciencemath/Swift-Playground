cb39f57008d41d518eec3778435e46ecf3f099ed
Mathais <matrixmachinelearning@gmail.com>
2023-07-23 12:51:39 -0400

    flash card swipe w/ animation

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Mathias on 7/23/23.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Whats the difference betwwen @State and @Published", answer: "@State updates current body, @Published waits for changes and announces to the world that its changed.")
+}
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -269,8 +269,9 @@ struct ContentView9: View {
                 //        .clipShape(Capsule())
             }
     }
+}
     
-struct ContentView: View {
+struct ContentView10: View {
     @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     
     var body: some View {
@@ -279,6 +280,42 @@ struct ContentView: View {
             .background(reduceTransparency ? .black : .black.opacity(0.5))
             .foregroundColor(.white)
             .clipShape(Capsule())
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+
+struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
